5b0eef5becad5ae2bb51b9909f454ce3cf8a163e
Yashwant <2018ucp1416@mnit.ac.in>
2020-07-24 17:38:09 +0530

    removed userPROFILEurl

--- app.js
+++ app.js
@@ -65,11 +65,14 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret:process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/blogsite",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
+
+  clientID: process.env.GOOGLE_CLIENT_ID,
+
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+     callbackURL:"http://localhost:3000/auth/google/secrets",
+
+   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id },{name:profile.displayName}, function (err, user) {
       return cb(err, user);
